57fb28740e99527e0ee65f223898b7fc59330b32
YoelK <yoelkastro@gmail.com>
2020-08-15 17:00:39 +0300

    Added dungeon generation

---
+++ src/Dungeon.js
@@ -0,0 +1,112 @@
+class Room {
+	
+	constructor(){
+		this.walls = {north: undefined, west: undefined, south: undefined, east: undefined};
+	}
+
+	initializeWallEvents(){
+
+	}
+
+}
+
+class Dungeon {
+
+	constructor(){
+		this.curFloor = new Array()
+	}
+
+	generateFloor(floorNo){
+		this.curFloor = new Array();
+
+		var totalRooms = 5 + (Math.pow(floorNo, 2));
+		var numFloors = 1;
+
+		let floor = Array.from(Array(totalRooms * 2 + 1), () => new Array(totalRooms * 2 + 1));
+
+		function generateRooms(x, y, curFloor){
+
+			var nextX, nextY;
+
+			var toGenerate = Math.floor((Math.random() * 30) / 9);
+			if(toGenerate == 0){toGenerate = 1}
+
+			for(var n = 0; n < toGenerate; n ++){
+
+				numFloors ++;
+				var dir = Math.floor(Math.random() * 4); // 0: north, 1: west, 2: south, 3: east
+				let triedDirs = new Array();
+
+				while(floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))] !== undefined){
+					triedDirs.push(dir);
+					dir = Math.floor(Math.random() * 4);
+					if(triedDirs.length > 3){
+
+						for(var x = 0; x < floor.length; x ++){
+							var done = false;
+							for(var y = 0; y < floor.length; y ++){
+
+								if(floor[x][y] !== undefined){
+									generateRooms(x, y, curFloor);
+									done = true;
+									break;
+								}
+
+							}
+							if(done){
+								break;
+							}
+						}
+						break;
+					}
+				}
+
+				floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))] = new Room();
+				nextX = x + ((dir % 2) * (dir - 2));
+				nextY = y + (((dir + 1) % 2) * (dir - 1));
+
+				curFloor.push(floor[nextX][nextY]);
+
+			}
+
+			if(numFloors < totalRooms){
+				generateRooms(nextX, nextY, curFloor);
+			}
+
+		}
+
+		floor[totalRooms][totalRooms] = new Room();
+		this.curFloor.push(floor[totalRooms][totalRooms]);
+
+		generateRooms(totalRooms, totalRooms, this.curFloor);
+
+		function checkForNeighbors(x, y){
+
+			for(var dir = 0; dir < 4; dir ++){
+				if(floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))] !== undefined){
+					switch(dir){
+						case 0: floor[x][y].walls.north = floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))]; break;
+						case 1: floor[x][y].walls.west = floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))]; break;
+						case 2: floor[x][y].walls.south = floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))]; break;
+						case 3: floor[x][y].walls.east = floor[x + ((dir % 2) * (dir - 2))][y + (((dir + 1) % 2) * (dir - 1))]; break;
+					}
+				}
+			}
+
+		}
+
+		for(var x = 0; x < floor.length; x ++){
+			for(var y = 0; y < floor.length; y ++){
+
+				if(floor[x][y] !== undefined){
+					checkForNeighbors(x, y);
+					floor[x][y].initializeWallEvents();
+				}
+
+			}
+		}
+		return this.curFloor;
+
+	}
+
+}
